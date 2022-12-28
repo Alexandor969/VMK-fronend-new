c4b83d07d9ae040c03ab6f7bef4c754d62e99cc5
alexpetrovdev <front@artgorka.ru>
2022-12-28 13:31:00 +0300

    4ru

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -33,12 +33,12 @@ instance.interceptors.response.use( (config: any) => {
         }).then((res: any) => {
             document.cookie = `access_token=${res.data.accessToken}; max-age=3600`
             localStorage.setItem("roles", res.data.user.roles)
+            return instance.request(originalRequest.headers.authorization = `Bearer ${getCookie('access_token')}`)
         }).catch((err: any) => {
             document.cookie = "access_token=;max-age=-1";
             localStorage.removeItem("roles")
             router.push({name: 'login'})
         })
-        return instance.request(originalRequest.headers.authorization = `Bearer ${getCookie('access_token')}`)
 
     }
     if(error.response.status === 401) {
